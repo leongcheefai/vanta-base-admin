cfec3b3ce72700934761369dfe1c62ffcb1f6706
Leong Chee Fai <leongcheefai0521@gmail.com>
2026-05-03 10:00:11 +0800

    feat: wrap app root with ThemeProvider

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -5,15 +5,18 @@ import '@praxor-kit/ui/styles'
 import { Toaster } from '@praxor-kit/ui'
 import { queryClient } from './lib/query'
 import { Router } from './router'
+import { ThemeProvider } from './lib/theme'
 
 const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('Root element #root not found')
 
 createRoot(rootEl).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
-    </QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <Toaster />
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
