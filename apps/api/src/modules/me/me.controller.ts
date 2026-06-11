e19dffa76a83dcfe87669e9982752d0ab682524a
Leong Chee Fai <leongcheefai0521@gmail.com>
2026-06-11 22:44:50 +0800

    feat(api): add NestJS me module

---
+++ apps/api/src/modules/me/me.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common'
+import { db } from '@vanta-base-admin/db'
+import { CurrentUser, type SessionUser } from '../../common/decorators/current-user.decorator'
+
+@Controller('me')
+export class MeController {
+  @Get()
+  getMe(@CurrentUser() user: SessionUser) {
+    return { user }
+  }
+
+  @Get('has-password')
+  async hasPassword(@CurrentUser() user: SessionUser) {
+    const account = await db.query.account.findFirst({
+      where: (acc, { and, eq }) =>
+        and(eq(acc.userId, user.id), eq(acc.providerId, 'credential')),
+    })
+    return { hasPassword: account !== undefined }
+  }
+}
