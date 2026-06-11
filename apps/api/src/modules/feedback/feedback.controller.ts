7392a24741bc96acac6fbd04f0c9d95b6b869d42
Leong Chee Fai <leongcheefai0521@gmail.com>
2026-06-11 22:48:43 +0800

    feat(api): add NestJS feedback module

---
+++ apps/api/src/modules/feedback/feedback.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common'
+import { CurrentUser, type SessionUser } from '../../common/decorators/current-user.decorator'
+import { CreateFeedbackDto } from './dto/create-feedback.dto'
+import { FeedbackService } from './feedback.service'
+
+@Controller('feedback')
+export class FeedbackController {
+  constructor(private readonly feedbackService: FeedbackService) {}
+
+  @Post()
+  create(@CurrentUser() user: SessionUser, @Body() dto: CreateFeedbackDto) {
+    return this.feedbackService.create(user.id, user.email, dto)
+  }
+}
